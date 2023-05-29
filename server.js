9a1b01edb902fe1c678515406074fdfd99a391a0
t0ru5 <bndct.dev@gmail.com>
2023-05-29 16:41:00 +1000

    10.2D Repo

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+const portNum = 4000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Setup the computational routes.
+const computeRoute = require('./routes/compute')
+app.use('/compute', computeRoute)
+//Use ejs 
+app.set('view engine', 'ejs')
+
+app.listen(portNum, console.log("Listening on port: ", portNum))
